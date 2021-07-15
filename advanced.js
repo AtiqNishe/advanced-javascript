63d4c0d5f10eb3e676e1ae45c39303fc413a2b10
Your   git config --global user.name Your <atiqnishebd24400@gmail.com>
2021-07-15 23:48:32 +0600

    Learning advanced javascript

---
+++ advanced.js
@@ -0,0 +1,19 @@
+// advanced javascript
+
+//let name = 6 ; condition is true;
+//let name = ""; condition = false; 
+//let name = " "; condition = is true;
+//let name = 0; condition = is false;
+//let name = "0"; condition = is true because string in 0;
+//let name = false; condition = is false;
+//let name = "false"; is true because string in false;
+//let name ; this variable value is kisu nai tai undefined mane false ;
+//let name = null; this variable value is null tai null hobe mane false hobe;
+ //let name = NaN; this variable value is NaN tai tai tar mane false hobe;
+// let name = 0; condition = is true because name || name===O; ar karone;
+if(name || name ===0){
+    console.log("condition is true", name );
+}
+else{
+    console.log("condition is false", name );
+}
